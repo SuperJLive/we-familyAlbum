9bcff2c261a5ff3d0956acc41376846c5ce7c593
SuperJ <superj@qq.com>
2021-08-30 14:33:18 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let capsule = wx.getMenuButtonBoundingClientRect();
+		if (capsule) {
+		 	this.globalData.Custom = capsule;
+			this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
+		} else {
+			this.globalData.CustomBar = e.statusBarHeight + 50;
+		}
+      }
+    })
+    return;
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res.code);
+        console.log(res);
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: this.globalData.baseUrl+'/wxprog/onlogin/'+res.code,
+            success (res) {
+              console.log(res.data)
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+    baseUrl: 'http://localhost:9000'
+
+
+  }
+})
